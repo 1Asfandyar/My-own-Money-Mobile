3bc9e433054e4339fb70d13359cc041916d78872
Abdur Rahman <khalil.ahmad1547@gmail.com>
2026-07-18 15:09:01 +0500

    fix: deployment changes

--- app.config.js
+++ app.config.js
@@ -1,13 +1,60 @@
 import 'dotenv/config';
 
-export default ({ config }) => ({
-  ...config,
-  plugins: [
-    ...(config.plugins ?? []),
-    'expo-secure-store',
-  ],
-  extra: {
-    apiUrl: process.env.EXPO_PUBLIC_API_URL,
-    appName: process.env.EXPO_PUBLIC_APP_NAME,
+export default {
+  expo: {
+    name: "My Own Money",
+    slug: "mom-money-manager",
+    version: "1.0.0",
+    orientation: "portrait",
+
+    icon: "./src/assets/icons/myownmoney_light.png",
+
+    scheme: "MyOwnMoney",
+    userInterfaceStyle: "light",
+
+    newArchEnabled: true,
+
+    experiments: {
+      typedRoutes: true,
+    },
+
+    splash: {
+      image: "./src/assets/icons/myownmoney_light.png",
+      resizeMode: "contain",
+      backgroundColor: "#1E5A52",
+    },
+
+    ios: {
+      supportsTablet: true,
+    },
+
+    android: {
+      package: "com.myownmoney.app",
+      adaptiveIcon: {
+        foregroundImage: "./src/assets/icons/myownmoney_light.png",
+        backgroundColor: "#1E5A52",
+      },
+      edgeToEdgeEnabled: true,
+    },
+
+    web: {
+      favicon: "./src/assets/icons/myownmoney_light.png",
+    },
+
+    plugins: [
+      "expo-router",
+      "expo-font",
+      "expo-secure-store",
+    ],
+
+    extra: {
+      apiUrl: process.env.EXPO_PUBLIC_API_URL,
+      appName: process.env.EXPO_PUBLIC_APP_NAME,
+      eas: {
+        projectId: "b5e9e61d-84d6-42a8-bdb8-d275463bd12a",
+      },
+    },
+
+    owner: "abdur1547s-organization",
   },
-});
+};
